ff1e431fdd9fbe13f3962f0fa82375a26bf23de1
Huda Aeda <hudaaeda10@gmail.com>
2021-03-19 08:34:40 +0700

    Pertemuan # 5 - Template Layouts

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg">
+            <h1>About Me</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo neque quae repellat laboriosam tenetur quo, aliquid libero, repudiandae quis omnis sit quaerat nam consectetur. Dolores quo eligendi necessitatibus laudantium dolore.</p>
+        </div>
+    </div>
+</div>
